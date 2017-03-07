91b9f22d8c848c4a7978dda433a9f53fc080af6f
Peter Lindholm <petlin@danskebank.com>
2017-03-07 14:10:16 +0100

    Initial commit

---
+++ GuidGenerator/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace GuidGenerator
+{
+    class Program
+    {
+        [DllImport("user32.dll")]
+        internal static extern bool OpenClipboard(IntPtr hWndNewOwner);
+
+        [DllImport("user32.dll")]
+        internal static extern bool CloseClipboard();
+
+        [DllImport("user32.dll")]
+        internal static extern bool SetClipboardData(uint uFormat, IntPtr data);
+
+        [STAThread]
+        static void Main(string[] args)
+        {
+            OpenClipboard(IntPtr.Zero);
+            var guid = Guid.NewGuid().ToString("D");
+            var ptr = Marshal.StringToHGlobalUni(guid);
+            SetClipboardData(13, ptr);
+            CloseClipboard();
+            Marshal.FreeHGlobal(ptr);
+        }
+    }
+}
